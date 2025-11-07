9198acf58929dc95f15effd1c4ccd1d12d80b814
Sckab <giulianodeamicis2009@gmail.com>
2025-11-07 23:42:59 +0100

    removed the `namespace fs = ...`

--- src/list_directories.cpp
+++ src/list_directories.cpp
@@ -4,12 +4,10 @@
 
 #include "../lib/list_directories.h"
 
-namespace fs = std::filesystem;
-
 std::vector<std::string> listDirectories(const std::string &path) {
     std::vector<std::string> directories;
 
-    for (const auto &entry : fs::directory_iterator(path)) {
+    for (const auto &entry : std::filesystem::directory_iterator(path)) {
         directories.push_back(entry.path().string());
     }
 
